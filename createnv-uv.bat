a233ccd5d5b1d2a15b366ba8a4ed5dcbad3a49d8
Andre Lima <andre.p.lima@gmail.com>
2026-02-04 12:30:43 -0300

    migrating from conda to uv

---
+++ createnv-uv.bat
@@ -0,0 +1,7 @@
+uv add shapely scikit-learn matplotlib ffmpeg
+uv add psutil 
+uv add scikit-fuzzy pyreadstat
+uv add bootstrapped
+uv add palmerpenguins
+uv add scikit-multilearn-ng
+uv add primefac
